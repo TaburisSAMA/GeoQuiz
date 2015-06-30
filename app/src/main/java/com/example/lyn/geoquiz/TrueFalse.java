07f8f4530d2482e277433f3176b001d81a067236
lyn <taburissama@gmail.com>
2015-06-30 16:25:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lyn/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.example.lyn.geoquiz;
+
+/**
+ * Created by lyn on 15-6-19.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private  boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion(){
+        return mQuestion;
+    }
+
+    public void setQuestion(int question){
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion(){
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion){
+        mTrueQuestion = trueQuestion;
+    }
+}
